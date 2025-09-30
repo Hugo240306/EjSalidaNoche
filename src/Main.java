59424e3c8179cceac2db958672ba809f43cb34e6
´´Hugo240306´´ <hugosuarezbarreiro06@liceolapaz.net>
2025-09-30 08:56:06 +0200

    Ejercicio 1 y 2

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+
+public class Main {
+    public static void main(String[] args) {
+        // 1. Mensaje de bienvenida
+        System.out.println("Vamos a tomar el aire. Bienvenido");
+
+        // 2. Variables
+        double nivelAlcohol = 0.0;
+        double precioChupito = 3;
+        double precioCaña = 1.8;
+        double precioCombinado = 8;
+
+        double nivelChupito = 0.25;
+        double nivelCaña = 0.1;
+        double nivelCombinado = 0.4;
+
+        // (opcional: mostrar las variables para comprobar)
+        System.out.println("Nivel de alcohol inicial: " + nivelAlcohol);
+        System.out.println("Precio chupito: " + precioChupito);
+        System.out.println("Precio caña: " + precioCaña);
+        System.out.println("Precio combinado: " + precioCombinado);
+        System.out.println("Nivel chupito: " + nivelChupito);
+        System.out.println("Nivel caña: " + nivelCaña);
+        System.out.println("Nivel combinado: " + nivelCombinado);
+    }
+}

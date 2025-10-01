3d9fda6e5d1c8cbac2f4a243d9d6d6da3420d41e
´´Hugo240306´´ <hugosuarezbarreiro06@liceolapaz.net>
2025-10-01 09:03:59 +0200

    Añadidos comentarios

--- src/Main.java
+++ src/Main.java
@@ -5,33 +5,36 @@ public class Main {
 
         double nivelAlcohol = 0.0;
         double precioChupito = 3;
-        double precioCaña = 1.8;
+        double precioCanha = 1.8;
         double precioCombinado = 8;
 
         double nivelChupito = 0.25;
-        double nivelCaña = 0.1;
+        double nivelCanha = 0.1;
         double nivelCombinado = 0.4;
 
         System.out.println("Nivel de alcohol inicial: " + nivelAlcohol);
         System.out.println("Precio chupito: " + precioChupito);
-        System.out.println("Precio caña: " + precioCaña);
+        System.out.println("Precio caña: " + precioCanha);
         System.out.println("Precio combinado: " + precioCombinado);
         System.out.println("Nivel chupito: " + nivelChupito);
-        System.out.println("Nivel caña: " + nivelCaña);
+        System.out.println("Nivel caña: " + nivelCanha);
         System.out.println("Nivel combinado: " + nivelCombinado);
 
         Scanner sc = new Scanner(System.in);
-
+// el system.in lo que hace es recoger la informacion escrita por el usuario
         System.out.print("Introduce el nombre del cliente: ");
         String nombre = sc.nextLine();
-
+// y el String nombre = sc.nextLine(); lo que hace es permitir que se cambie el nombre cada vez que se inicia
         System.out.print("Introduce la edad del cliente: ");
         int edad = sc.nextInt();
+// int es para numeros enteros
+        if (edad < 18) {
+            System.out.println("No puedes entrar. Eres menor de edad.");
 
         System.out.print("Introduce la cantidad de dinero que tendrá el cliente: ");
         double dinero = sc.nextDouble();
 
-        System.out.println("\n--- Datos del cliente ---");
+        System.out.println("--- Datos del cliente ---");
         System.out.println("Nombre: " + nombre);
         System.out.println("Edad: " + edad);
         System.out.println("Dinero: " + dinero + " €");
@@ -41,8 +44,7 @@ public class Main {
                 " tiene " + dinero + " euros.");
 
 
-        if (edad < 18) {
-            System.out.println("No puedes entrar. Eres menor de edad.");
+
             return;
         } else if (nivelAlcohol > 1.2) {
             System.out.println("No puedes entrar. No estás en condiciones (alcohol > 1.2).");
@@ -50,13 +52,6 @@ public class Main {
         } else {
             System.out.println("Bienvenido a la discoteca, " + nombre);
         }
-
-
-
-
-
-
-
 
     }
 
